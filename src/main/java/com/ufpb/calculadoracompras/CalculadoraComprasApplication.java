00facdd17f1eaffcc8b213ff2a6eb99efdc2bcf3
Ademilson Silva <ademilson.w6@hotmail.com>
2022-11-20 19:01:27 -0300

    Initial commit

---
+++ src/main/java/com/ufpb/calculadoracompras/CalculadoraComprasApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.calculadoracompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraComprasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraComprasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufpb/calculadoracompras/CalculadoraComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.calculadoracompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraComprasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
